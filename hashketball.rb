f3fb8bdaedaf2b5febe643c0e97e0c6e343e6dfa
Alice Wang <aywang6@gmail.com>
2020-04-02 21:53:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -58,8 +58,6 @@ def game_hash
       },
       ]
   }, :away => {
-    :team_name => "Charlotte Hornets",
-    :colors => ["Turquoise", "Purple"],
     :players => [
       {:player_name => "Jeff Adrien",
         :number => 4,
